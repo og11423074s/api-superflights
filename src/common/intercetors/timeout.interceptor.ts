ec40166e9d07d584f0b7a2080f485d7cb5985e08
Osmar Gonzalez <osmarlenin.gonzalezescorche@externos-cl.cencosud.com>
2025-06-17 12:39:21 -0300

    feat: add timeout interceptor with 120s limit for all requests

---
+++ src/common/intercetors/timeout.interceptor.ts
@@ -0,0 +1,13 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { Observable, timeout } from "rxjs";
+
+
+
+
+export class TimeoutInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, 
+        next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        return next.handle().pipe(timeout(120000))
+    }
+
+}
